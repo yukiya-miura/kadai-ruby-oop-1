31b73e0685848b837a75db837c1567510d4d55fd
yukiya.miura <2511kagurazaka@gmail.com>
2021-07-06 10:51:36 +0000

    retry

--- team.rb
+++ team.rb
@@ -13,7 +13,7 @@ class Team
       return win.to_f / (win + lose)
   end
   
-  def show_team_result(name)
+  def show_team_result
       puts "#{name} の2020年の成績は #{win}勝 #{lose}敗 #{draw}分、勝率は #{calc_win_rate}です。"
   end
 end
@@ -28,9 +28,9 @@ Hiroshima = Team.new('Carp',52,56,12)
 Yakuruto = Team.new('Swallows',41,69,10)
 
 # インスタンスの使用
-Kyojin.show_team_result("Giants")
-Hannshin.show_team_result("Tigers")
-Chuunichi.show_team_result("Dragons")
-Yokohama.show_team_result("BayStars")
-Hiroshima.show_team_result("Carp")
-Yakuruto.show_team_result("Swallows")
+Kyojin.show_team_result
+Hannshin.show_team_result
+Chuunichi.show_team_result
+Yokohama.show_team_result
+Hiroshima.show_team_result
+Yakuruto.show_team_result
