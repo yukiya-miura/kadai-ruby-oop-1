750f05d604f57ef7ba42aacf9b0a1ec84389bdc9
yukiya.miura <2511kagurazaka@gmail.com>
2021-07-06 10:35:57 +0000

    retry

--- team.rb
+++ team.rb
@@ -12,7 +12,6 @@ class Team
   def calc_win_rate
       return win.to_f / (win + lose)
   end
- 
   
   def show_team_result(name)
       puts "#{name} の2020年の成績は #{win}勝 #{lose}敗 #{draw}分、勝率は #{calc_win_rate}です。"
@@ -21,17 +20,17 @@ end
 
 # インスタンスの生成と、変数への代入
 
-Giants = Team.new('Giants',67,45,8)
-Tigers = Team.new('Tigers',60,53,7)
-Dragons = Team.new('Dragons',60,55,5)
-BayStars = Team.new('BayStars',56,58,6)
-Carp = Team.new('Carp',52,56,12)
-Swallows = Team.new('Swallows',41,69,10)
+Kyojin = Team.new('Giants',67,45,8)
+Hannshin = Team.new('Tigers',60,53,7)
+Chuunichi = Team.new('Dragons',60,55,5)
+Yokohama = Team.new('BayStars',56,58,6)
+Hiroshima = Team.new('Carp',52,56,12)
+Yakuruto = Team.new('Swallows',41,69,10)
 
 # インスタンスの使用
-Giants.show_team_result('Giants')
-Tigers.show_team_result("Tigers")
-Dragons.show_team_result("Dragons")
-BayStars.show_team_result("BayStars")
-Carp.show_team_result("Carp")
-Swallows.show_team_result("Swallows")
+Kyojin.show_team_result("Giants")
+Hannshin.show_team_result("Tigers")
+Chuunichi.show_team_result("Dragons")
+Yokohama.show_team_result("BayStars")
+Hiroshima.show_team_result("Carp")
+Yakuruto.show_team_result("Swallows")
